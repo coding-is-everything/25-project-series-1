f0a9fc8e4ba94b65d815f748fe911c3cade96744
The Batraa Numerology <thebatraanumerology@gmail.com>
2025-06-25 10:30:32 +0530

    feat(modal): add custom modal component with test implementation
    Add a reusable modal component with customizable header, body and footer.
    Includes test component to demonstrate usage and basic styling with animations.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Accordian from "./components/accordian";
+import ModalTest from "./components/custom-modal-popup/modal-test";
 
 function App() {
   return (
     <div className="App">
-      <Accordian />
+      {/* <Accordian /> */}
+      <ModalTest />
     </div>
   );
 }
