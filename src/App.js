765022246deede4f3cdbeaef4e46b402e3ef41ad
The Batraa Numerology <thebatraanumerology@gmail.com>
2025-06-25 11:36:39 +0530

    feat(feature-flags): implement feature flag system with context and demo components
    Add feature flag system with global state management using React context.
    Includes dummy API service, context provider, and demo component that conditionally renders features based on flags.
    Currently enables accordion and tabs components as examples.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import "./App.css";
 import Accordian from "./components/accordian";
 import ModalTest from "./components/custom-modal-popup/modal-test";
 import TabTest from "./components/custom-tabs/tab-test";
+import FeatureFlags from "./components/feature-flag";
+import FeatureFlagGlobalState from "./components/feature-flag/context";
 
 function App() {
   return (
     <div className="App">
-      {/* <Accordian /> */}
-      {/* <ModalTest /> */}
-      <TabTest />
+      <FeatureFlagGlobalState>
+        {/* <Accordian /> */}
+        {/* <ModalTest /> */}
+        {/* <TabTest /> */}
+        <FeatureFlags />
+      </FeatureFlagGlobalState>
     </div>
   );
 }
